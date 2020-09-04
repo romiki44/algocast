590cc6bcadeac8d40498e10458666cee56581316
Roman Kiss <romiki44@hotmail.com>
2020-09-04 20:32:10 +0200

    reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  if(!str) {
+    return str;
+  }
+  let arr=[];
+  for (let i =str.length;  i >= 0; i--) {
+    arr.push(str[i-1]);
+  }
+  return arr.join('');
+}
 
 module.exports = reverse;
+
+
